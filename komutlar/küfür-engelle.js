a5b1303c481cb1b250c70d46e4b1e90de395469d
Glitch (hello-express) <none>
2019-07-25 17:16:40 +0000

    🌧🎗 Checkpoint ./komutlar/küfür-engelle.js:2045523/910 ./server.js:2045523/12498 ./ayarlar.json:2045523/421

---
+++ komutlar/küfür-engelle.js
@@ -0,0 +1,31 @@
+const db = require('quick.db')
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args) => {
+  if (!args[0]) return message.channel.send(' Aç yada Kapat yaz! Örnek: !küfürengel aç')
+  if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('`SUNUCUYU_YÖNET` yetkisine sahip olmalısın!')
+
+  if (args[0] == 'aç') {
+    db.set(`kufur_${message.guild.id}`, 'Açık').then(i => {
+      message.channel.send(' Görünüşe Göre Küfür Koruması Zaten Aktif Dostum :) \n Kick Yetkisi Olanları Engellemez.')
+    })
+  }
+  if (args[0] == 'kapat') {
+    db.set(`kufur_${message.guild.id}`, 'Kapalı').then(i => {
+      message.channel.send('Küfür Koruması Devredışı Bırakılmıştır.')
+    })
+  }
+
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['küfür'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'küfür-engelle',
+  description: 'küfürengel',
+  usage: 'küfür'
+};
--- server.js
+++ server.js
@@ -1,22 +1,138 @@
-// server.js
-// where your node app starts
+ 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+const Jimp = require('jimp');
+const db = require('quick.db');
+const DBL = require('dblapi.js')
+require('./util/eventLoader')(client);
 
-// init project
-const express = require('express');
-const app = express();
+var prefix = ayarlar.prefix;
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(aliases => {
+      client.aliases.set(aliases, props.help.name);
+    });
+  });
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+  client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+
+
+client.on("message", async msg => {
+  db.fetch(`kufur_${msg.guild.id}`).then(i => {
+if (i == 'Açık') {
+        const kufur = ["amk","a.m.k","am","a.m","m.k","mk","orosbu çocugu","orospu çocugu","o.ç","oç","oc","o.c","orosbu","orospu","veledi zina","sikerim","sıkerım","s.i.k.e.r.i.m","s.ı.k.e.r.ı.m","piç","pıc","p.i.ç","p.ı.c","orosbu evladı","orospu evladı","amına koyayım","babanı sikim"];
+        if (kufur.some(word => msg.content.includes(word))) {
+          try {
+             if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  
+     
+               
+               msg.delete(); 
+
+
+                  return msg.reply('Sunucuda küfür filtresi aktif.').then(msg => msg.delete(3000));
+             }
+          } catch(err) {
+            console.log(err);
+          }
+        } } else if (i == 'Kapalı') {
+
+}
+
+})
 });
+
+
+
+
+//////////////////
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+
+
+client.login(ayarlar.token);
