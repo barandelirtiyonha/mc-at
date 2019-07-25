93983393bc3a37dc76502fd420ba62082941c67f
Glitch (cs-at) <none>
2019-07-25 18:18:52 +0000

    📒😻 Checkpoint ./ayarlar.json:2045523/59 ./events/ready.js:2045523/47 ./komutlar/küfüraç.js:2045523/1

--- events/ready.js
+++ events/ready.js
@@ -8,8 +8,8 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
   console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("idle");
-  client.user.setGame(`Testing Bot | XiR`);
+  client.user.setStatus("oline");
+  client.user.setGame(`Yakında...`);
   console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
-//XiR
+
