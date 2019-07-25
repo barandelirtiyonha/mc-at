f71f3e187907630b744209a1ca7e758b7c88ce6d
Glitch (cs-at) <none>
2019-07-25 19:45:48 +0000

    🗺⭐️ Checkpoint ./komutlar/otorol-yardım.js:2045523/16 ./komutlar/otorol.js:2045523/872 ./bot.js:2045523/273 ./komutlar/yardım.js:2045523/36

--- bot.js
+++ bot.js
@@ -109,7 +109,17 @@ client.unload = command => {
 };
 
 
+///////////////////////////////////////////////////////////
 
+client.on('guildMemberAdd', member => {
+  db.fetch(`autoRole_${member.guild.id}`).then(i => {
+ try {
+ member.addRole(member.guild.roles.find("name", i))
+} catch (e) {
+ console.log('Rol veremedim...')
+}
+})
+});
 
 ///////////////////////////////////////////////////////////
 
--- komutlar/otorol-yardım.js
+++ komutlar/otorol-yardım.js
@@ -1,5 +1,5 @@
 exports.run = function(client, message, args) {
-        message.channel.send("<:yanlis:604033229653147667> Ayarlamam İçin Bir Rol Etiketlemeilisin. \n  Rolü Etiketleyemiyorsan Rolün Etiketleme Seçeneğini Aktif Etmeyi Unutma *  *Örnek Kullanım : !otorol @rol #kanal ");
+        message.channel.send("<:yanlis:604033229653147667> Ayarlamam İçin Bir Rol Etiketlemeilisin. \n  Rolü Etiketleyemiyorsan **Rolün Etiketleme Seçeneğini Aktif Etmeyi Unutma** \n Örnek Kullanım : !otorol-ayarla @rol #kanal ");
     };
 
 module.exports.conf = {
