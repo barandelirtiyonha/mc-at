b6fc09f81f4f09a33bd5aaf428079fcbd4dfe1cb
Glitch (cs-at) <none>
2019-07-25 19:56:47 +0000

    🖖🚵 Checkpoint ./komutlar/sil.js:2045523/758 ./utils/errors.s:2045523/1475 ./bot.js:2045523/3537

---
+++ komutlar/sil.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  if(!args[0]) return message.reply(" En Az \`1 - 5000\` Arasında Bir Tam Sayı Değeri Girmelisiniz.");
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send(`Temizlendi **${args[0]}** .`).then(msg => msg.delete(5000));
+  });
+}
+
+module.exports.help = {
+  name: "sil"
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+};
+
+exports.help = {
+  name: 'sil',
+  description: '',
+  usage: '',
+}
