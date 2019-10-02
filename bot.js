9a09e01fb3b030b91a8c8e91ac21689b3fb86953
Glitch (mighty-adapter) <none>
2019-10-02 18:57:21 +0000

    👠🐴 Checkpoint ./package.json:1355552/142729 ./komutlar/level-sistemi.js:1355552/25127 ./bot.js:1355552/1322

--- bot.js
+++ bot.js
@@ -285,39 +285,6 @@ if (i == 'Açık') {
 });
 
 ////////////////////////////////////////////////////////////
-////////////////////////////////
-client.on("message", async msg => {
-  const db = require('quick.db');
-  if (msg.channel.type === "dm") return;
-  if(msg.author.bot) return;  
-  
-  if (msg.content.length > 7) {
-    
-    db.add(`puancik_${msg.author.id + msg.guild.id}`, 1)
-};
-
-  if (db.fetch(`puancik_${msg.author.id + msg.guild.id}`) > 150) {
-    
-    db.add(`seviye_${msg.author.id + msg.guild.id}`, 1)
-    if (msg.guild.id === "264445053596991498") return;
-    msg.channel.send(`${client.emojis.get(client.emoji.levelup)}Tebrik ederim <@${msg.author.id}>! Seviye atladın ve **${db.fetch(`seviye_${msg.author.id + msg.guild.id}`)}** seviye oldun!`)
-    db.delete(`puancik_${msg.author.id + msg.guild.id}`)};
- 
-  if (db.has(`roll_${msg.guild.id}`) === true) {
-  if (db.has(`rollss_${msg.guild.id}`) === true) {
-    
- var r = db.fetch(`roll_${msg.guild.id}`)
- var s = db.fetch(`rollss_${msg.guild.id}`)
-  
-  if (db.fetch(`seviye_${msg.author.id + msg.guild.id}`) == s) {
-    if (msg.member.roles.has(msg.guild.roles.get(r).id) === false) {
-    msg.channel.send(`<@${msg.author.id}> başarıyla **${db.fetch(`seviye_${msg.author.id + msg.guild.id}`) - 1 || 0}** seviyeyi geçtin ve **${msg.guild.roles.get(r).name}** rolünü aldın!`)
-    msg.member.addRole(msg.guild.roles.get(r).id)
-    }
-  };
-}};
-});
-////////////////////////
 
 
 client.elevation = message => {
