05e5f9bdbcb1efaa4f97733153b5ea4bd3c43d6d
Glitch (mighty-adapter) <none>
2019-10-04 22:46:20 +0000

    🐺🍇 Checkpoint ./komutlar/yardım.js:1426658/368

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,8 +7,8 @@ exports.run = (client, message, args) => {
     .setColor('#fffa00')
     .setAuthor(`MC-AT `, client.user.avatarURL) 
       .setDescription('**!Yardım** ile yardım alabilirsiniz.')
-      .addField(`örnek komut kullanımı: !küfüraç. \n` + '')
-      .addField(`botu davet etmek için !davet.yard \n` + '')
+      .addField('örnek komut kullanımı: **!küfüraç.**')
+      .addField('botu davet etmek için **!davet**')
       .addField('** !Komutlar (12)**', `herkesin kullanabileceği standart komutlar. \n` +  '`bot bilgi`, `davet`, `gold`, `webpanel`, `sunucupp`, `profil`, `kredim`, `p-Market`, `p-Menü`, `ping`, `puanım`, `ayarlar`')
       .addField('** !Eğlence (9)**',   `herkes için kullanılabilecek eğlence komutları. \n` + '`kralol`, `maymunol`, `yılanol`, `adamol`, `kediol`, `aşk`, `token`, `tokat`')
       .addField('** !Moderasyon (12)**',`yetkililer için moderasyon komutları bölüm . \n` +  '`küfür`, `reklam`, `link engel`, `kick`,`reklam isim ban`,`otorol`,`sayaç`,`ban koruma sistemi`,`oto cevap`,`mute-sistemi`,`anti-raid-bot-sistemi`')
@@ -17,7 +17,7 @@ exports.run = (client, message, args) => {
     .setTimestamp()
     message.channel.send(embedyardim).catch()
     
-//bnm işim bu kadar kendin herşeyi editle <3 <3
+
 };
 
 exports.conf = {
