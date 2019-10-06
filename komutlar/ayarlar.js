92e0ffb1e7cbe9c166b3a88aec0dd73617a45b7f
Glitch (cotton-sandpaper) <none>
2019-10-06 20:25:22 +0000

    🎢🐚 Checkpoint ./komutlar/ayarlar.js:1426658/6370

--- komutlar/ayarlar.js
+++ komutlar/ayarlar.js
@@ -1,30 +1,37 @@
 const Discord = require('discord.js');
-const db = require('quick.db');
+var ayarlar = require('../ayarlar.json');
+const fs = require('fs');
+let gkanal = JSON.parse(fs.readFileSync("./ayarlar/glog.json"));
+let gkanal2 = JSON.parse(fs.readFileSync("./ayarlar/glog1.json"));
+let küfürEngel = JSON.parse(fs.readFileSync("././jsonlar/kufurEngelle.json", "utf8"));
 
-exports.run = async (client, message, args, member) => {
 
-  let kufurfiltre = await db.fetch(`kufur_${message.guild.id}`)
-  let kufurYazi;
-  if (kufurfiltre == null) kufurYazi = 'Küfür filtresi açık değil, ayarlamak için `!küfür-filtresi aç`'
-  if (kufurfiltre == 'açık') kufurYazi = 'Küfür filtresi açık.'
-  if (kufurfiltre == 'kapali') kufurYazi = 'Küfür filtresi açık değil, ayarlamak için `!küfür-filtresi aç`'
+exports.run = (client, message) => {
   
-const ayarlar = new Discord.RichEmbed()
-.setTitle('Ayarlar:')
-.addField('Küfür engelleme', kufurYazi)
-.setColor('BLUE')
-message.channel.send(kufurfiltre)
-}
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu komutu kullanabilmek için **Yönetici** iznine sahip olmalısın!`);
+  
+  var embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setAuthor(`» ${message.guild.name} Sunucu Ayarları «`, `https://images-ext-2.discordapp.net/external/ujBwHK3NkvSNyffbh50uDXaOIG6M3ED77fYEsn33Sl4/http/cdn.onlinewebfonts.com/svg/download_136247.png`)
+
+
+  .addField("Küfür Engelleme Sistemi", küfürEngel[message.guild.id] ? "Açık" : "Kapalı" ,true)
+
+  .addField("Küfür Engelleme Sistemi", gkanal[message.guild.id] ? "Açık" : "Kapalı" ,true)
+  .addField("bb-kanal", gkanal[message.guild.id] ? `**Mesaj:** \n\`\`\`${gkanal[message.guild.id].gkanal}\`\`\`` : `Ayarlanmamış`)
+  message.channel.send(embed)
+};
 
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  permLevel: 0,
-};
-
-exports.help = {
-  name: 'ayarlar',
-  description: 'Sunucuya ayarlanan komutları gösterir.',
-  usage: 'ayarlar'
-};
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: `Yönetici izni gerekiyor.`
+  };
+  
+  exports.help = {
+    name: 'ayarlar',
+    category: 'ayarlar',
+    description: 'Sunucu ayarlarını gösterir.',
+    usage: 'a!ayarlar'
+  };
