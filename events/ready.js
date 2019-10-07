ec9710ca299c9adb1d4f99d3452b523a8b7eb8cf
Glitch (cotton-sandpaper) <none>
2019-10-07 21:17:38 +0000

    🍣💕 Checkpoint ./events/ready.js:1426658/1138

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,21 @@
+const chalk = require('chalk');
+const moment = require('moment');
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
 
 module.exports = async client => {
-  client.appInfo = await client.fetchApplication();
+    client.appInfo = await client.fetchApplication();
     setInterval( async () => {
       client.appInfo = await client.fetchApplication();
     }, 60000);
-    
-    require("../util/dashboard.js")(client);
+  
+  console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("oline");
+  client.user.setGame(`🔥!yılbaşı🔥!gold 🔥 !yardım 🔥 web https://kc-at-web.glitch.me/ 🔥 BETA  0.9.3 `);
+  console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  require("../util/dashboard.js")(client);
 };
+
