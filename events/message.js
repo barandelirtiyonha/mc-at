5957daa3ed6b122548e7fbde05872002e1cf6887
Glitch (kc-at-panel) <none>
2019-10-08 21:43:21 +0000

    🌿💘 Checkpoint ./komutlar/emojiler.js:1426658/2455 ./komutlar/yardım.js:1426658/11 ./events/message.js:1426658/1648 ./dashboard/templates/blocks/footer.ejs:1426658/500

--- events/message.js
+++ events/message.js
@@ -1,10 +1,17 @@
 const ayarlar = require('../ayarlar.json');
-module.exports = async message => {
+let talkedRecently = new Set();
+module.exports = message => {
+  if (talkedRecently.has(message.author.id)) {
+    return;
+  }
+  talkedRecently.add(message.author.id);
+    setTimeout(() => {
+    talkedRecently.delete(message.author.id);
+  }, 2500);
   let client = message.client;
-  let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(' ')[0].slice(prefix.length);
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
   let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
   let cmd;
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,7 +10,7 @@ exports.run = (client, message, args) => {
       .addField('** !Komutlar (12)**', `herkesin kullanabileceği standart komutlar. \n` +  '`bot bilgi`, `davet`, `gold`, `webpanel`, `sunucupp`, `profil`, `kredim`, `p-Market`, `p-Menü`, `ping`, `puanım`, `ayarlar`,`yılbaşı`')
       .addField('** !Eğlence (9)**',   `herkes için kullanılabilecek eğlence komutları. \n` + '`kralol`, `maymunol`, `yılanol`, `adamol`, `kediol`, `aşk`, `token`, `tokat`')
       .addField('** !Moderasyon (12)**',`yetkililer için moderasyon komutları bölüm . \n` +  '`küfür`, `reklam`, `link engel`, `kick`,`reklam isim ban`,`otorol`,`sayaç`,`ban koruma sistemi`,`oto cevap`,`mute-sistemi`,`anti-raid-bot-sistemi`,`sunucu-panel`')
-      .addField('** !Moderasyon2 (12)**',`yetkililer için moderasyon komutları bölüm . \n` + '`sil`,`reklam-taraması`,`resimli hg-bb`,`sunucutanıt`,`oto bot silici`,`ultra sohbet temizleyici`,`slowmode`,`tag Sistemi`,`rol-sistemi`,`geçici-oda`,`kayıt sistemi`,`!erkek kayıt`')
+      .addField('** !Moderasyon2 (12)**',`yetkililer için moderasyon komutları bölüm . \n` + '`sil`,`reklam-taraması`,`resimli hg-bb`,`sunucutanıt`,`oto bot silici`,`ultra sohbet temizleyici`,`slowmode`,`tag Sistemi`,`rol-sistemi`,`geçici-oda`,`kayıt sistemi`,`!erkek kayıt`,`emojiler`')
     .setFooter(`© ${client.user.username} ` , client.user.avatarURL)
     .setTimestamp()
     message.channel.send(embedyardim).catch()
