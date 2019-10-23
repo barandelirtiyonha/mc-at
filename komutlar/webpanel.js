0e81574a476fc38e8b2a91deb115f3e820e665fc
Glitch (kc-at-panel) <none>
2019-10-23 19:54:23 +0000

    🛋🏭 Checkpoint ./komutlar/gold-üye-yardım.js:1426658/2510 ./komutlar/webpanel.js:1426658/1392

---
+++ komutlar/webpanel.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+//
+
+exports.run = (client, message, params) => {
+  const embedyardim = new Discord.RichEmbed()
+  .setTitle(`${client.user.username}`)
+  .setDescription('')
+  .setColor("WebPanel")
+    .setDescription('')
+      .setDescription('Hep Daha İleriye Bizimle! \n\n**Harika Özellikler İçeren web panel Sistemi Hakkında!** \n\n Bizlere Destek Olmak Ve Harika Özellikler Kazanmak İçin [Tıkla](https://discord.gg/UnZjvxu)')
+      .setFooter(`© ${client.user.username}` )
+
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    message.channel.send(embedyardim);
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.author.send('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+    }
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['gold','Gold'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'webpanel',
+  description: 'webpanel i gosterir.',
+  usage: 'webpanel '
+};
